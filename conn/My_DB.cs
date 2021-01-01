7965e1675761b72587c1c4aa294292289af734e8
KhangNguyen <giakhangnguyen115@gmail.com>
2021-01-01 23:06:00 +0700

    model

---
+++ conn/My_DB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnDBMS.conn
+{
+    class My_DB
+    {
+        SqlConnection con = new SqlConnection(@"");
+
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+        public void openConnection()
+        {
+            if ((con.State == System.Data.ConnectionState.Closed))
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if ((con.State == System.Data.ConnectionState.Open))
+            {
+                con.Close();
+            }
+        }
+    }
+}
